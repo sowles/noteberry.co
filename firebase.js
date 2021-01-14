2399f28900a9d2c6e635749c5fb533b160dbb570
Minty Milk <mintymilk1@gmail.com>
2021-01-13 21:56:46 -0500

    firebase added

---
+++ firebase.js
@@ -0,0 +1,23 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.2.2/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.2.2/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyD4NllxC19g6oj4_s2AN8s4JBls1mjXODA",
+    authDomain: "noteberry-92ba5.firebaseapp.com",
+    projectId: "noteberry-92ba5",
+    storageBucket: "noteberry-92ba5.appspot.com",
+    messagingSenderId: "114450424733",
+    appId: "1:114450424733:web:ff8b8c058b6168bc04c574",
+    measurementId: "G-CK9BJVGRKX"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
