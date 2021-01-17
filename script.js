2d38415c35e38477939ea601b20cb7aa4bf16c46
Minty Milk <mintymilk1@gmail.com>
2021-01-16 21:21:57 -0500

    Script.js refactor

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 let firebaseDB;
+var provider = new firebase.auth.GoogleAuthProvider();
 
 //integration of Google login
-firebase.auth().signInWithRedirect(provider);
+// firebase.auth().signInWithRedirect(provider);
 // console.log(2);
 // console.log(auth());
+
+document.querySelector("#loginBtn").addEventListener("click", () => {
+  firebase.auth()
+    .signInWithPopup(provider)
+    .then((result) => {
+      /** @type {firebase.auth.OAuthCredential} */
+      var credential = result.credential;
+
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      var token = credential.accessToken;
+      // The signed-in user info.
+      var user = result.user;
+      // ...
+    }).catch((error) => {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      // The email of the user's account used.
+      var email = error.email;
+      // The firebase.auth.AuthCredential type that was used.
+      var credential = error.credential;
+      // ...
+    });
+})
